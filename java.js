a214748870b4d8f5b30b1ae3d60c4d7de9176cd1
Umanohara <132902417+Umanohara@users.noreply.github.com>
2023-06-03 12:41:27 +0530

    Add files via upload

---
+++ java.js
@@ -0,0 +1,17 @@
+var loader=document.querySelector(".loader");
+window.addEventListener("load",vanish);
+function vanish(){
+    loader.classList.add("hidden");
+
+}
+
+var prevScrollpos = window.pageYOffset;
+window.onscroll = function() {
+  var currentScrollPos = window.pageYOffset;
+  if (prevScrollpos > currentScrollPos) {
+    document.getElementById("navbar").style.top = "0";
+  } else {
+    document.getElementById("navbar").style.top = "-50px";
+  }
+  prevScrollpos = currentScrollPos;
+}
